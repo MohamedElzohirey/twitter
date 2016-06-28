ab2a5519f322ef85218d1a625c969d096f3bb49e
mohamed_elzohirey <mohamed.elzohirey@gmail.com>
2016-06-28 09:45:57 +0200

    init commit

---
+++ twitter/TWAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TWAppDelegate.h
+//  twitter
+//
+//  Created by Mohamed Elzohirey on 6/28/16.
+//  Copyright © 2016 Eventus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  twitter
+//
+//  Created by Mohamed Elzohirey on 6/28/16.
+//  Copyright © 2016 Eventus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
