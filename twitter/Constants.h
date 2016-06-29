fc89fb9de806b596886c96ca094bcb3486dade1f
mohamed_elzohirey <mohamed.elzohirey@gmail.com>
2016-06-29 23:38:18 +0200

    adding constant file

---
+++ twitter/Constants.h
@@ -0,0 +1,28 @@
+//
+//  Constants.h
+//  twitter
+//
+//  Created by Mohamed Elzohirey on 6/28/16.
+//  Copyright © 2016 Eventus. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#endif /* Constants_h */
+#define kLoginViewController @"Login"
+#define kFollowersViewController @"Followers"
+#define kFollowerDetailsController @"FollowerDetails"
+#define kTWFollowerCell @"TWFollowerCell"
+#define kTWTweetCell @"TWTweetCell"
+#define kAPIBaseURL @"https://api.twitter.com/1.1/"
+#define kFollowers kAPIBaseURL @"followers/list.json?user_id=%@&count=20&cursor=%@"
+#define kTimeLine kAPIBaseURL @"statuses/user_timeline.json?user_id=%@&count=10"
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0f \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0f \
+blue:((float)(rgbValue & 0xFF))/255.0f \
+alpha:1.0f]
+
+#define kBlueColorActive UIColorFromRGB(0x3e65cc)
